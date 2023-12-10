150c076f19486f9eb40e8de2a8cef72ae63a98b4
AINUS <50290684+ainusdev@users.noreply.github.com>
2023-12-11 01:11:15 +0800

    Update main_header layout and content positioning
    Transformed the main_header component's layout from inline to flex and enhanced content alignment for better visual presentation. The changes affected CSS properties, flex direction, alignment, and gap of elements within the header, and also included the repositioning of ProfileIcon within

--- components/main_header.js
+++ components/main_header.js
@@ -2,14 +2,15 @@ import styles from './main_header.module.css'
 
 import Link from "next/link";
 import ProfileIcon from "@/components/profile_icon";
-import Menu from "@/components/menu";
 import Image from "next/image";
 
 export default function Main_header({children}) {
     return (
         <header className={styles.headerContainer}>
             <Link href={"/"}><Image src={"/Logo.png"} alt={"logo"} width={336 / 4} height={145 / 4}/></Link>
-            <ProfileIcon label="+"/>
+            <div className={styles.headerContainer2}>
+                <ProfileIcon label="+"/>
+            </div>
         </header>
     )
 }
