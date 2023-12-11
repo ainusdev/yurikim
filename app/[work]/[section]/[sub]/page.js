ea2dee9f4ca40a568824aa3468ff369700f810d6
AINUS <50290684+ainusdev@users.noreply.github.com>
2023-12-12 07:04:25 +0800

    Remove data.json from app directory, update data.json in data directory
    This commit removes the file data.json from the app directory, indicating a file relocation. Additionally, data.json in the data directory has been updated with new image paths and sequence navigation elements. The changes seen in app's layout.js and page.js files are for the redesigned navigation and image display structure of the application.

--- app/[work]/[section]/[sub]/page.js
+++ app/[work]/[section]/[sub]/page.js
@@ -1,7 +1,43 @@
-export default function SubPage({ params }) {
+import Image from "next/image";
+import data from "@/data/data.json";
+import Link from "next/link";
+
+export default function SubPage({params}) {
     const work = params.work;
     const section = params.section;
     const sub = params.sub;
-    const currentPath = `/${work}/${section}/${sub}`;
-    return <div>My Path: /{work}/{section}/{sub}</div>
+    const currentSection = `/${work}/${section}`;
+    const currentPath = `${currentSection}/${sub}`;
+    const subData = data[work][section][sub];
+    const subAddressList = Object.keys(data[work][section]).filter(e => e !== "meta")
+
+    const currentIndex = subAddressList.indexOf(sub)
+
+    const isFirst = currentIndex === 0;
+    const isLast = currentIndex === subAddressList.length - 1;
+
+    const prevAddress = `${currentSection}/${isFirst ? subAddressList[subAddressList.length - 1] : subAddressList[currentIndex - 1]}`;
+    const nextAddress = `${currentSection}/${isLast ? subAddressList[0] : subAddressList[currentIndex + 1]}`;
+
+    return <>
+        <Image src={`/${subData.meta.top}`} alt={"top"} layout={"responsive"} width={10} height={10}/>
+        <h1>{subData.meta.title_ko}</h1>
+        {
+            !subData.meta.bottom
+                ? <></>
+                : <Image src={`/${subData.meta.bottom}`} alt={"bottom"} layout={"responsive"} width={10} height={10}/>
+
+        }
+
+
+
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "1em"
+        }}>
+            <Link href={prevAddress}>Prev</Link>
+            <Link href={nextAddress}>Next</Link>
+        </div>
+    </>
 }
--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,6 @@ export default function RootLayout({children}) {
     return (
         <html>
         <body>
-        <Path/>
         {children}
         </body>
         </html>
